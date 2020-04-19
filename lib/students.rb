c9d1af18fe658ce5e39b1b3e8481b3eb533e9bb2
Kyle Imhoff <imhoff.kyle@gmail.com>
2020-04-19 19:38:47 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -5,7 +5,7 @@ def highest_student_gpa
 end
 
 def lowest_student_gpa
-  "SELCECT MIN(gpa) as lowest_gpa FROM students;"
+  "SELCECT MIN(gpa) as lowest_student_gpa FROM students;"
 end
 
 def average_student_gpa
